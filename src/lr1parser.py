f1931c89cb9345b62d9d96ec9a20147347697ac7
Никита <118850355+ascnd1ng@users.noreply.github.com>
2025-10-11 01:08:18 +0300

    prepare tree build for lr1parse

--- src/lr1parser.py
+++ src/lr1parser.py
@@ -4,19 +4,19 @@ lr1table = {
     (0, 'i'): 's4',
 
     (1, '+'): 's5',
-    (1, '$'): 'f',
+    (1, 'END'): 'f',
 
     (2, '+'): ['r1', 'E', 'T'],
     (2, '*'): 's6',
-    (2, '$'): ['r1', 'E', 'T'],
+    (2, 'END'): ['r1', 'E', 'T'],
 
     (3, '+'): ['r3', 'T', 'F'],
     (3, '*'): ['r3', 'T', 'F'],
-    (3, '$'): ['r3', 'T', 'F'],
+    (3, 'END'): ['r3', 'T', 'F'],
 
     (4, '+'): ['r5', 'F', 'i'],
     (4, '*'): ['r5', 'F', 'i'],
-    (4, '$'): ['r5', 'F', 'i'],
+    (4, 'END'): ['r5', 'F', 'i'],
 
     (5, 'i'): 's4',
 
@@ -24,11 +24,11 @@ lr1table = {
 
     (7, '+'): ['r2', 'E', 'E+T'],
     (7, '*'): 's6',
-    (7, '$'): ['r2', 'E', 'E+T'],
+    (7, 'END'): ['r2', 'E', 'E+T'],
 
     (8, '+'): ['r4', 'T', 'T*F'],
     (8, '*'): ['r4', 'T', 'T*F'],
-    (8, '$'): ['r4', 'T', 'T*F']
+    (8, 'END'): ['r4', 'T', 'T*F']
 }
 
 goto = {
@@ -77,14 +77,14 @@ class LR1Parse:
 
     def parse(self):
         result = list()
-        # self.magazine.append('$')
+        # self.magazine.append('END')
         self.magazine.append(0)
         token_ind = 0
         a = self.tokens[token_ind]
 
         while 1:
             s = self.magazine[-1]
-            print(self.magazine, end= ', ')
+            print(self.magazine, end=', ')
             cur_action = self.action[(s, a)]
             print(cur_action)
             if is_shift(cur_action):
@@ -105,6 +105,43 @@ class LR1Parse:
                 raise ValueError(f"LR(1) parse ERROR")
 
 
-lr1 = LR1Parse('i+i*i$', lr1table, goto)
+tokens = list('i+i*i')
+tokens.append('END')
+lr1 = LR1Parse(tokens, lr1table, goto)
 res = lr1.parse()
 print(res)
+
+# Заготовка построения дерева
+
+# class Node:
+#     def __init__(self, value, children=None):
+#         self.value = value
+#         self.children = children or []  # только названия детей
+#
+#     def __repr__(self):
+#         return f"{self.value}: {self.children}"
+
+
+
+# rules = [['r5', 'F', 'i'], ['r3', 'T', 'F'], ['r1', 'E', 'T'],
+#          ['r5', 'F', 'i'], ['r3', 'T', 'F'], ['r5', 'F', 'i'],
+#          ['r4', 'T', 'T*F'], ['r2', 'E', 'E+T']]
+#
+# stack = []
+#
+# for rule in rules:
+#     _, lhs, rhs = rule
+#     rhs_symbols = list(rhs)
+#     children = []
+#
+#     for symbol in rhs_symbols[::-1]:
+#         if stack:
+#             child_node = stack.pop()
+#             children.insert(0, child_node.value)
+#         else:
+#             children.insert(0, symbol)
+#
+#     stack.append(Node(lhs, children))
+#
+# tree_root = stack[0]
+# print(tree_root)
