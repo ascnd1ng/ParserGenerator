c0fda266482ec7ee6150c0c73913292bab3e2c9d
Никита <118850355+ascnd1ng@users.noreply.github.com>
2025-10-26 01:03:56 +0300

    add scripts for lr1

---
+++ scripts/lr1_check_calc.py
@@ -0,0 +1,79 @@
+from generation.gen_calc_lr1_table import LR1
+from scripts.make_table_generation import lr1_make_table_generation
+from src.constants import *
+
+i_p = '../grammar_descriptions/arithmetic_expression.txt'
+g_p = '../generation/graph312expression.dot'
+t_p = None
+
+pt = [LR1()]
+patterns = patterns_calc
+axiom = 'E1'
+
+root = lr1_make_table_generation(pt, i_p, g_p, t_p, patterns, axiom)
+
+res = []
+
+
+def calc(root, res):
+    if root.value in ['*', '+', 'n', '(', ')']:
+        res.append(root.attr)
+        return
+    for c in root.children:
+        calc(c, res)
+
+
+calc(root, res)
+expr = ''.join(res)
+
+
+def calculate_expression(s):
+    def evaluate(tokens):
+        stack = []
+        num = 0
+        sign = '+'
+
+        while tokens:
+            token = tokens.pop(0)
+
+            if token.isdigit():
+                num = int(token)
+
+            if token == '(':
+                num = evaluate(tokens)
+
+            if not token.isdigit() or not tokens:
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '*':
+                    stack.append(stack.pop() * num)
+
+                sign = token
+                num = 0
+
+                if token == ')':
+                    break
+
+        return sum(stack)
+
+    tokens = []
+    i = 0
+    while i < len(s):
+        if s[i] == ' ':
+            i += 1
+            continue
+        if s[i] in '()+*':
+            tokens.append(s[i])
+            i += 1
+        else:
+            num_str = ''
+            while i < len(s) and s[i].isdigit():
+                num_str += s[i]
+                i += 1
+            tokens.append(num_str)
+
+    return evaluate(tokens)
+
+
+result = calculate_expression(expr)
+print(result)
