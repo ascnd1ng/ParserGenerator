779e8b32c839e2d063c667150d844a94cf23b2cd
Никита <118850355+ascnd1ng@users.noreply.github.com>
2025-10-26 16:45:25 +0300

    s

--- scripts/make_table_generation.py
+++ scripts/make_table_generation.py
@@ -4,39 +4,6 @@ from src.lr1table import LR1Nt, LR1T, LR1Rules, LR1Axiom
 from src.lr1table_builder import LR1ParserTableBuilder
 
 
-def make_table_generation(pt, i_p, g_p, t_p, patterns, axiom):
-    with open(i_p, 'r') as f:
-        text = f.read()
-    tokens = lexer(text, patterns)
-    print(tokens)
-
-    # root = TopDownParse(tokens, pt).parse(axiom)
-    lr1parser = LR1Parser(tokens, pt)
-    root = lr1parser.parse_all()
-    with open(g_p, 'w') as f:
-        f.write('digraph {\n')
-        root.print_graph(f)
-        f.write('}')
-
-    if t_p is None:
-        return root
-
-    g = Grammar()
-    g.extract(root)
-    nts, ts, rules, axioms = g.get()
-    print(g)
-
-    checker = Checker(nts, ts, rules, axioms, tokens)
-    checker.check()
-
-    fff = FirstFollowFinder(nts, ts, rules, axioms)
-    first, follow = fff.find()
-
-    t = Table(nts, ts, rules, first, follow)
-    t.compute_table()
-    t.gen_file(t_p)
-
-
 def generate_lr1_class(action_data, goto_data, file_path):
     with open(file_path, 'w') as file:
         file.write("class LR1:\n")
@@ -71,7 +38,7 @@ def lr1_make_table_generation(pt, i_p, g_p, t_p, patterns, axiom):
     print(tokens)
 
     # root = TopDownParse(tokens, pt).parse(axiom)
-    lr1parser = LR1Parser(tokens, [LR1Nt(), LR1T(), LR1Rules(), LR1Axiom()])
+    lr1parser = LR1Parser(tokens, pt)
     root = lr1parser.parse_all()
     with open(g_p, 'w') as f:
         f.write('digraph {\n')
